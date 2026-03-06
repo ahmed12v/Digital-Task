005f1ff11ffa7163fbf297b102337ba33bbf5b31
ahmed12v <ahnurmed16@gmail.com>
2026-03-07 01:42:39 +0200

    About+Numbars+Services

--- src/app/components/about-us/about-us.ts
+++ src/app/components/about-us/about-us.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
@@ -7,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.css',
 })
 export class AboutUs {
+
+   stats = [
+    {id:1, label: 'Satisfied Clients', target: 120, current: 0 },
+    {id:2, label: 'Success Partners', target: 50, current: 0 },
+    {id:3, label: 'Creative Ideas', target: 300, current: 0 },
+    {id:4, label: 'Pending Projects', target: 15, current: 0 }
+  ];
+  trackById(index: number, item: any): number {
+  return item.id;
+}
+
+  counterStarted = false;
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const section = document.querySelector('.numbers-section');
+    if (section && !this.counterStarted) {
+      const top = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      if (top <= windowHeight * 0.8) { 
+        this.startCounting();
+        this.counterStarted = true;
+      }
+    }
+  }
+
+  startCounting() {
+    this.stats.forEach(stat => {
+      const increment = Math.ceil(stat.target / 100); 
+      const interval = setInterval(() => {
+        if (stat.current < stat.target) {
+          stat.current += increment;
+          if(stat.current > stat.target) stat.current = stat.target;
+        } else {
+          clearInterval(interval);
+        }
+      }, 20); 
+    });
+  }
 
 }
