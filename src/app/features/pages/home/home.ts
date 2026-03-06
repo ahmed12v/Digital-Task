0fb3ace2d09a48359ccce38eddb2f089a00f232e
ahmed12v <ahnurmed16@gmail.com>
2026-03-06 23:58:29 +0200

    navbar

--- src/app/features/pages/home/home.ts
+++ src/app/features/pages/home/home.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../../layout/navbar/navbar';
+import { Footer } from '../../../layout/footer/footer';
+import { AboutUs } from '../../../components/about-us/about-us';
+import { ContactUs } from '../../../components/contact-us/contact-us';
+import { HeroSection } from '../../../components/hero-section/hero-section';
+import { Reviews } from '../../../components/reviews/reviews';
+import { OurServices } from '../../../components/our-services/our-services';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Navbar, Footer, AboutUs, ContactUs, HeroSection, Reviews, OurServices],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -8,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class Navbar {
 
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    if (this.menuOpen) {
+    document.body.classList.add('no-scroll');
+  } else {
+    document.body.classList.remove('no-scroll');
+  }
+  }
+  closeMenu() {
+    this.menuOpen = false;
+    document.body.classList.remove('no-scroll');
+  }
+
+ 
+
 }
