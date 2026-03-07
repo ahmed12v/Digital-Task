ab2d3e7680286c36ba05510dd0b342a8d3f4c00a
ahmed12v <ahnurmed16@gmail.com>
2026-03-07 03:36:56 +0200

    end-Sections

--- src/app/components/contact-us/contact-us.ts
+++ src/app/components/contact-us/contact-us.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.css',
 })
 export class ContactUs {
+
+   contactForm!: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.contactForm.invalid) return;
+
+    //console.log('Form Value:', this.contactForm.value);
+    this.contactForm.reset();
+    this.submitted = false;
+    alert('Message sent successfully!');
+  }
 
 }
